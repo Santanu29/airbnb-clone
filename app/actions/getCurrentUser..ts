79adf963bee9f2975be8f15793153084db664c3e
santanu29 <santanubiswas2905@gmail.com>
2024-02-06 15:11:18 +0530

    auth

---
+++ app/actions/getCurrentUser..ts
@@ -0,0 +1,40 @@
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+import { User } from "@prisma/client";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser: User = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    // Some issues with the toISOString(). So I have just returned the string right now
+    return {
+      ...currentUser,
+      createdAt: currentUser.createAt?.toISOString(),
+      updatedAt: currentUser.updatedAt?.toISOString(),
+      emailVerfied: currentUser?.emailVerfied?.toISOString() || null,
+    };
+  } catch (error: any) {
+    console.log("error has occured in catch", error);
+    return null;
+  }
+}
