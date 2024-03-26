5ac2cc8da27ecb65b3e623f7bcdf7434478904e7
santanu29 <santanubiswas2905@gmail.com>
2024-03-26 23:59:48 +0530

    final commit

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,7 +1,21 @@
-import { User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
+
+export type SafeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
 
 export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
+};
+
+export type SafeReservations = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
 };
