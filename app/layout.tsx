26f8f115b259796a0e0bde9abf07b5b23c6ada01
santanu29 <santanubiswas2905@gmail.com>
2024-03-27 09:50:57 +0530

    Revert "final commit"
    This reverts commit 5ac2cc8da27ecb65b3e623f7bcdf7434478904e7.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,14 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/navbar/ClientOnly";
-import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
+
 import LoginModal from "./components/modals/LoginModal";
-import getCurrentUser from "./actions/getCurrentUser.";
 import RentModal from "./components/modals/RentModal";
+import RegisterModal from "./components/modals/RegisterModal";
+import SearchModal from "./components/modals/SearchModal";
+
+import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -33,9 +36,10 @@ export default async function RootLayout({
           <LoginModal />
           <RegisterModal />
           <RentModal />
+          <SearchModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
